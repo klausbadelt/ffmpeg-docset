12c69f2aaa81c85d03c49b17c4b222400c8add7d
Klaus Badelt <info@klausbadelt.com>
2015-06-23 05:38:42 +0200

    remove parked code

--- generate.sh
+++ generate.sh
@@ -4,14 +4,12 @@ rm -rf _output/ffmpeg.tgz
 mkdir -p _output/ffmpeg.docset/Contents/Resources/Documents
 
 # copy into docset
-# cp -rf _input/ffmpeg/doc/*.html _output/ffmpeg.docset/Contents/Resources/Documents
 cp -rf _input/ffmpeg/doc/*.css _output/ffmpeg.docset/Contents/Resources/Documents
 cp -rf _input/icon*.png _output/ffmpeg.docset
 cp -rf _input/Info.plist _output/ffmpeg.docset/Contents/Info.plist
 
 # index
 ruby index.rb
-#find doc -name \*\.html | xargs -n 1 ruby ~/Codes/erlang.docset/src/generate.rb | sqlite3 ../docSet.dsidx
 
 # compress
 cd _output
